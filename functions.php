ac0caf75b01c570e149e5d56dfd38681dc7e8705
martindwyer <martin@mdbytes.com>
2023-01-02 13:56:05 -0600

    Events post type

--- functions.php
+++ functions.php
@@ -4,6 +4,7 @@ function university_files()
 {
     wp_enqueue_script('main-university-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
     wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+    wp_enqueue_style('google-font-for-logo', '//fonts.googleapis.com/css2?family=MuseoModerno:ital,wght@0,400;0,500;0,600;0,700;0,800;1,500&display=swap');
     wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
     wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
     wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
@@ -13,10 +14,30 @@ add_action('wp_enqueue_scripts', 'university_files');
 
 function university_features()
 {
-    register_nav_menu('header-menu-location','Header Menu Location');
-    register_nav_menu('footer-location-1','Footer location 1');
-    register_nav_menu('footer-location-2','Footer location 2');
+    register_nav_menu('header-menu-location', 'Header Menu Location');
+    register_nav_menu('footer-location-1', 'Footer location 1');
+    register_nav_menu('footer-location-2', 'Footer location 2');
     add_theme_support('title-tag');
 }
 
 add_action('after_setup_theme', 'university_features');
+
+function university_adjust_queries($query)
+{
+    $today = date('Ymd');
+    if (!is_admin() && is_post_type_archive('event') && $query->is_main_query()) {
+        $query->set('meta-key', 'event_date');
+        $query->set('orderby', 'meta_value_num');
+        $query->set('order', 'ASC');
+        $query->set('meta_query', array(
+            array(
+                'key' => 'event_date',
+                'compare' => '>=',
+                'value' => $today,
+                'type' => 'numeric'
+            )
+        ));
+    }
+}
+
+add_action('pre_get_posts', 'university_adjust_queries');
--- header.php
+++ header.php
@@ -2,8 +2,8 @@
 <html <?php language_attributes(); ?>>
 
 <head>
-    <meta charset="<?php bloginfo('charset'); ?>" >
-    <meta name="viewport" content="width=device-width, initial-scale=1" >
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php wp_head() ?>
 </head>
 
@@ -11,7 +11,7 @@
     <header class="site-header">
         <div class="container">
             <h1 class="school-logo-text float-left">
-                <a href="<?php echo site_url() ?>"><strong>BU Technical</strong> College</a>
+                <a href="<?php echo site_url() ?>"><strong>CollegePress</strong> <span class="logo-font">md</span></a>
             </h1>
             <span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
             <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
